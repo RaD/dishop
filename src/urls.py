cd66f5ed7207896a8d047a7c7da5ba8930cb0e74
Ruslan Popov <ruslan.popov@gmail.com>
2011-09-14 03:02:56 +0400

    Sitemap.

--- src/urls.py
+++ src/urls.py
@@ -4,23 +4,34 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.contrib.sitemaps import Sitemap, FlatPageSitemap, GenericSitemap
+
+from shop import models
 
 admin.autodiscover()
 
 urlpatterns = patterns('')
+
+sitemaps = {
+    'flatpages': FlatPageSitemap,
+    'items': GenericSitemap({
+        'queryset': models.Product.objects.all(),
+        'date_field': 'registered',
+        }, priority=0.6),
+    }
 
 if settings.DEBUG:
     urlpatterns += patterns(
     '',
     # разрешаем вывод документации по проекту
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
 
 urlpatterns += patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^search/', include('haystack.urls')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
